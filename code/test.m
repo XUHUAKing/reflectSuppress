6506613d652bc4e0a4e9eca9bcd929c0253771ba
King <xhuangat@connect.ust.hk>
2019-10-24 13:47:33 +0800

    testable version of reflectSuppress

--- code/test.m
+++ code/test.m
@@ -1,8 +1,8 @@
-fileList = dir('../figures/*.png');
+fileList = dir('../save_png/*.png');
 for idx = 1:length(fileList)
     file = fileList(idx);
     savedir = fullfile('../results/', file.name);
-    datadir = fullfile('../figures/', file.name);
+    datadir = fullfile('../save_png/', file.name);
     Im = imread(datadir);
     T = reflectSuppress(Im, 0.033, 1e-8);
     Gray = rgb2gray(T); 
@@ -12,9 +12,9 @@ for idx = 1:length(fileList)
     Gray(Gray>65535) = 65535;
     imwrite(Gray, savedir);
 end
-subplot(1,3,1); 
-imshow(Im); 
-subplot(1,3,2); 
-imshow(T);
-subplot(1,3,3); 
-imshow(Gray);
+% subplot(1,3,1); 
+% imshow(Im); 
+% subplot(1,3,2); 
+% imshow(T);
+% subplot(1,3,3); 
+% imshow(Gray);
--- npy2png.py
+++ npy2png.py
@@ -1,3 +1,12 @@
+'''
+convert .npy to .png (non-hierachical format)
+
+steps:
+    1. download ./crop_npy folder
+    2. run this script python npy2png.py
+    3. output will be stored in ./save_png
+    4. run matlab script test.m
+'''
 import numpy as np
 from glob import glob
 import cv2
@@ -23,6 +32,7 @@ for folder2 in folders2[::5]:
 print('Data load succeed!')
 
 for m_name in val_items:
+    import pdb; pdb.set_trace()
     m_img = 0.5*np.load(m_name)[0,:,:,-1]
     h,w = m_img.shape[:2]
     h = h // 32 *32
@@ -36,10 +46,14 @@ for m_name in val_items:
     m_img = np.power(m_img[:,:,np.newaxis], 1/2.2)
     m_img = np.tile(m_img,[1,1,3])
     # new png name
-    save_path = m_name.replace(datadir, savedir).replace('.npy', '.png')
+    # save_path = m_name.replace(datadir, savedir).replace('.npy', '.png')
+    save_path = m_name.replace('.npy', '.png')
+    path_list = save_path.split('/')
+    img_name = path_list[-4]+'-'+path_list[-3]+'-'+path_list[-2]+'-'+path_list[-1]
+    save_path = savedir + '/' + img_name
     # scale back to 65535
     # save_img = cv2.cvtColor((m_img * 255.).clip(0,65535), cv2.COLOR_BGR2GRAY).astype(np.uint16) # np.uint16 for save, np.uint8 for visualize
     save_img = (m_img * 65535.).clip(0,65535).astype(np.uint16) # save as RGB
-    if not os.path.exists(save_path[:-12]):
-        os.makedirs(save_path[:-12])
+    # if not os.path.exists(save_path[:-12]):
+    #     os.makedirs(save_path[:-12])
     cv2.imwrite(save_path, save_img)
